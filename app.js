d325c80fbb6cbf4b6ea6e4accc8a925136f35b35
Mariana Schmale <marianaschmal@yahoo.dem>
2022-05-19 19:03:34 +0200

    again

--- app.js
+++ app.js
@@ -49,6 +49,16 @@ app.use("/api/profile", profile);
 
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
+
+
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 module.exports = app;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,5 @@
 const router = require("express").Router();
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
-});
-
 
 
 // You put the next routes here 👇
