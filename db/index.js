3fd56075a8b457c026e8a63eab61fc01cfb83354
Mariana Schmale <marianaschmal@yahoo.dem>
2022-05-19 17:52:36 +0200

    Update xyz.js with new middleware

--- db/index.js
+++ db/index.js
@@ -2,7 +2,10 @@
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
 
-// ℹ️ Sets the MongoDB URI for our app to have access to it.
+require('dotenv').config();
+
+
+// ℹ️ Sets the DB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost/theFinalProject";
